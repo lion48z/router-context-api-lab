ed4918a65621569bdb2d4a85ef9fe1d4718d9f34
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-28 18:48:49 -0500

    Added not found stuff
    Add a Cat

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      Uh Oh! We Can't Find What You're Looking For.
+    </div>
+  )
+}
+
+export default NotFoundPage
