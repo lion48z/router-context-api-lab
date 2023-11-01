20ff27836e2877f0a2b7d421b4a50eb0d41af811
lion48z <140863653+lion48z@users.noreply.github.com>
2023-11-01 18:36:44 -0500

    working on functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <div>
-      
+      <input></input>
+      <button>Search</button>
     </div>
   )
 }
--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const MovieContext = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const searchMovie = () =>{
+
+  }
   return (
     <div>
       
