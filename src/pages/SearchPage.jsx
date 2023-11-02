0d2599ecc6390ef9afe1650b4e6b9e440293581b
lion48z <140863653+lion48z@users.noreply.github.com>
2023-11-01 21:25:33 -0500

    Update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import SearchBar from '../components/SearchBar'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SearchBar from '../components/SearchBar';
+import MovieProvider from '../context/MovieContext';
 const SearchPage = () => {
   return (
     <div>
+      <MovieProvider>
       <SearchBar/>
       {/*<MovieList/>*/}
+      </MovieProvider>
       <Outlet/>
     </div>
   )
